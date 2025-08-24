47651dae063958e2f71408b26b21c11bed6f9c9b
YukkuriC <799433638@qq.com>
2025-08-24 21:10:32 +0800

    PnC pressure chamber disenchanter

---
+++ remote/code/pnc/disenchanter_chamber.lua
@@ -0,0 +1,44 @@
+-- input peripheral names
+local input, equipments, raw_output, books = table.unpack(arg)
+raw_output = peripheral.wrap(raw_output)
+
+if #arg < 4 then
+    print('USAGE: disenchanter_chamber <input> <equipments> <raw_output> <books>')
+    exit()
+end
+
+local function doPush(target, slot, name)
+    print(string.format('pushing %s to %s', item, target))
+end
+
+print('STARTED...')
+-- recycle output
+while 1 do
+    local hasInput = false
+    for i = 1, 27 do
+        local item = raw_output.getItemDetail(i)
+        if item then
+            hasInput = true
+            local target = nil
+            if item.enchantments then
+                if item.name == 'minecraft:enchanted_book' and #item.enchantments == 1 then
+                    -- put books to slot1
+                    target = books
+                else
+                    -- recycle enchantments to output
+                    target = input
+                end
+            elseif item.name == 'minecraft:book' then
+                target = input
+            else
+                -- put equipments to slot2
+                target = equipments
+            end
+            print(string.format('%s -> %s', item.name, target))
+            raw_output.pushItems(target, i)
+        end
+    end
+    if not hasInput then
+        sleep(0.5)
+    end
+end
